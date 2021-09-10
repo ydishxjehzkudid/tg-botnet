7262aa06d92c0ecbf686d36de9340b21dee13769
ydishxjehzkudid <90040456+ydishxjehzkudid@users.noreply.github.com>
2021-09-11 02:43:39 +0300

    Update register.py

--- functions/register.py
+++ functions/register.py
@@ -2,3 +2,10 @@ from telethon.sync import TelegramClient
 
 def main(api_id, api_hash):
     phone = input("[?] phone number ➜ ")
+    session = phone.replace("+", "")
+    client = TelegramClient(session,
+    api_id, api_hash)
+    print("[!] sending verify code")
+    client.sign_in(phone)
+    code = input("[?] verify code ➜ ")
+    client.sign_in(phone, code)
