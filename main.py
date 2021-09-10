7f99a59440cbed94f188b65dd3d48000cea67792
ydishxjehzkudid <90040456+ydishxjehzkudid@users.noreply.github.com>
2021-09-11 02:25:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ for filename in os.listdir("sessions"):
 
 with open("config.toml") as file:
     config = toml.load(file)
+
 authorization = config["authorization"]
+api_id, api_hash = authorization["api_id"], authorization["api_hash"]
 
 update = input("update sessions?
 sessions = os.listdir("sessions")
-api_id, api_hash = authorization["api_id"], authorization["api_hash"]
 
 while True:
     
