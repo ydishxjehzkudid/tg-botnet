e0742f256ea1a174af20e02c0ef4583440ad56cc
ydishxjehzkudid <90040456+ydishxjehzkudid@users.noreply.github.com>
2021-09-11 03:29:32 +0300

    Create flood.py

---
+++ functions/flood.py
@@ -0,0 +1,36 @@
+from threading import Thread
+import os, sys, asyncio
+from telethon.sync import TelegramClient, events
+from telethon import functions
+
+class chat(Thread):
+    def __init__(self, name, api_id, api_hash, trigger):
+        self.name = "sessions/" + name
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.trigger = trigger
+        Thread.__init__(self)
+    def main(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        client = TelegramClient(session_name=self.name, api_id=self.api_id,
+        api_hash=self.api_hash)
+        client.connect()
+        
+        @client.on(events.NewMessage):
+        async def flood(message):
+            if message.text != self.trigger:
+                return
+            count = 0
+            while True:
+                try:
+                    msg = await client(functions.messages.SendMessageRequest(
+                    peer=message.chat_id, message="test")
+                    count += 1
+                    print(f"[{msg.sender_id}] sended [count: {count}]")
+                except Exception as error:
+                    try:
+                        me = message.client.get_me()
+                        print(f"[{me.id}] not sended [error: {error}]")
+                    except Exception as err:
+                        print(str(error) + str(err))
