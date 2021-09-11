5d21f95666280e9f6bfffc8ea3430ac79999c28c
ydishxjehzkudid <90040456+ydishxjehzkudid@users.noreply.github.com>
2021-09-11 03:33:57 +0300

    Update flood.py

--- functions/flood.py
+++ functions/flood.py
@@ -11,26 +11,31 @@ class chat(Thread):
         self.trigger = trigger
         Thread.__init__(self)
     def main(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        client = TelegramClient(session_name=self.name, api_id=self.api_id,
-        api_hash=self.api_hash)
-        client.connect()
+        try:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            client = TelegramClient(session_name=self.name, api_id=self.api_id,
+            api_hash=self.api_hash)
+            client.connect()
         
-        @client.on(events.NewMessage):
-        async def flood(message):
-            if message.text != self.trigger:
-                return
-            count = 0
-            while True:
-                try:
-                    msg = await client(functions.messages.SendMessageRequest(
-                    peer=message.chat_id, message="test")
-                    count += 1
-                    print(f"[{msg.sender_id}] sended [count: {count}]")
-                except Exception as error:
+            @client.on(events.NewMessage):
+            async def flood(message):
+                if message.text != self.trigger:
+                    return
+                count = 0
+                while True:
                     try:
-                        me = message.client.get_me()
-                        print(f"[{me.id}] not sended [error: {error}]")
-                    except Exception as err:
-                        print(str(error) + str(err))
+                        msg = await client(functions.messages.SendMessageRequest(
+                        peer=message.chat_id, message="test")
+                        count += 1
+                        print(f"[{msg.sender_id}] sended [count: {count}]")
+                    except Exception as error:
+                        try:
+                            me = message.client.get_me()
+                            print(f"[{me.id}] not sended [error: {error}]")
+                        except Exception as err:
+                            print(str(error) + str(err))
+            client.start()
+            client.run_until_disconnected()
+        except Exception as error:
+            print(error)
